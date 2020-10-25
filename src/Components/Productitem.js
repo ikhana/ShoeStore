0bb904685cc660439fc3c800a3b585fbcd6f4184
ikhana <inaamullah484@gmail.com>
2020-10-25 04:44:20 -0700

    images alternatives removed

--- src/Components/Productitem.js
+++ src/Components/Productitem.js
@@ -11,7 +11,7 @@ function ProductItem() {
   return (
     <div  className="productitem" >
     <h3>{shoe.name}</h3>
-    <img src={shoe.img} height={300} alt="picture  from pixabay"></img>
+    <img src={shoe.img} height={300} alt="pixabay"></img>
     </div>
   );
 }
