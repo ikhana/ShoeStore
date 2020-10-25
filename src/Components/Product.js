ac9f97f4feceebd24c22ae26c03237ffad7c4d4f
ikhana <inaamullah484@gmail.com>
2020-10-25 04:38:41 -0700

    remove image

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -14,7 +14,7 @@ function Product() {
                    const shoe =Shoes[keyName];
                    return(<Link key={keyName} className="link" to={`product/${keyName}`}>
                        <h3>{shoe.name}</h3>
-                       <img src={shoe.img} height={200} alt="Shoes image"></img>
+                       <img src={shoe.img} height={200} alt="Shoes "></img>
                        </Link>
                    )
                })}
